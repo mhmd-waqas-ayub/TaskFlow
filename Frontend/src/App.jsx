ff5365ca2ac1836fdf93478b6ff7b59aed82882a
mhmd-waqas-ayub <waqasayub7951352@gmail.com>
2026-05-17 17:35:22 +0500

    fix: removed self-import circular dependency in Dashboard

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,8 @@
+import Dashboard from "./pages/Dashboard";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
+
 import ProtectedRoute from "./components/ProtectedRoute";
 import ProjectDetails from "./pages/ProjectDetails";
 
