1d0036cd1a9353de5fb075f92fad400d4ef4055c
mhmd-waqas-ayub <waqasayub7951352@gmail.com>
2026-05-18 16:51:55 +0500

    fix railway production binding

--- Backend/server.js
+++ Backend/server.js
@@ -96,7 +96,7 @@ const PORT = process.env.PORT || 5000;
 // app.listen(PORT,'127.0.0.1',()=>{
 //     console.log(`Server running on: http://127.0.0.1:  ${PORT}`);
 // });
-server.listen(PORT,  ()=>{
+server.listen(PORT,0,0,0,0,  ()=>{
     console.log(`Server is running on:http://localhost:${PORT}`);
     console.log("Socket.io is Initialized and listening")
 });
