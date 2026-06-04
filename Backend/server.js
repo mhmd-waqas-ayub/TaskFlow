ef066dca56c4d92db9535656b5ef2c19cf13f5ab
mhmd-waqas-ayub <waqasayub7951352@gmail.com>
2026-06-04 08:02:12 +0500

    fix railway deployment flow

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,8 @@
 const dotenv = require("dotenv");
-dotenv.config();
+if (process.env.NODE_ENV !== "production") {
+    dotenv.config();
+}
+
 
 const express = require("express");
 const cors = require("cors");
